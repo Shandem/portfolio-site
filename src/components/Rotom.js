7ba3f37a54054012b3b7817ef2a72ba73e7e5f25
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2020-07-07 15:18:13 -0700

    trying to fix font7

--- src/components/Rotom.js
+++ src/components/Rotom.js
@@ -4,30 +4,30 @@ import './Thecity.css'
 const Rotom = () => {
     return (
         <div className='tracker'>
-            <div class="rotom_full">
-            <div class="dextopC">
-                <div class="dexneck-tip"></div>
-                <div class="dexneck-right"></div>
-                <div class="dexneck"></div>
+            <div className="rotom_full">
+            <div className="dextopC">
+                <div className="dexneck-tip"></div>
+                <div className="dexneck-right"></div>
+                <div className="dexneck"></div>
             </div>
-            <div class="dexbody">
-                <div class="rotom_head">
-                    <div class="left_eye">
-                        <div class="left_slit"></div>
-                        <div class="left_eyeris"></div>
+            <div className="dexbody">
+                <div className="rotom_head">
+                    <div className="left_eye">
+                        <div className="left_slit"></div>
+                        <div className="left_eyeris"></div>
                     </div>
-                    <div class="right_eye">
-                        <div class="right__slit"></div>
-                        <div class="right_eyeris"></div>
+                    <div className="right_eye">
+                        <div className="right__slit"></div>
+                        <div className="right_eyeris"></div>
                     </div>
-                    <div class="rotom_mouth">
-                        <div class="rotom_mouth_left"></div>
-                        <div class="rotom_mouth_center"></div>
-                        <div class="rotom_mouth_right"></div>
+                    <div className="rotom_mouth">
+                        <div className="rotom_mouth_left"></div>
+                        <div className="rotom_mouth_center"></div>
+                        <div className="rotom_mouth_right"></div>
                     </div>
                 </div>
             </div>  
-                <div class="dextail"></div>
+                <div className="dextail"></div>
             </div>
         <p className='rotom-blurb'>This is another bit of CSS art. His name is Rotom from the Pokemon franchise. I wanted to 
             take on something more difficult than the  Pokeball, and with this project focused on making a Poke-dex clone 
