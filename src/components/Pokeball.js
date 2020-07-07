c28dd3723aec733b9d68edbbe677784d4b5bfaee
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2020-07-07 10:54:23 -0700

    Adds spinning pokeball

--- src/components/Pokeball.js
+++ src/components/Pokeball.js
@@ -3,13 +3,14 @@ import './Thecity.css'
 
 const Pokeball = () => {
     return (
-        <div className='cssart'>  
+        <div className='cssart'>
+            <div className="pokeball">  
                 <div id="pokeball">
                     <div id="layer1">
                     <div id="center"></div>
-                        <div id="bar"></div>
                         <div id="bottom-half"></div>
                     </div>  
+                </div>
             </div>
             <p className='blurb'>Here is some custom CSS art I put together. It is a recreation of 
             Nintendo's iconic Pokeball from the Pokemon franchise. I have provided
