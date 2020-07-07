dd8a39038d0dd63d84886dd75fc0146cbd6de536
Joshuah Gutierrez <gutierrezjoshuah@gmail.com>
2020-07-06 17:28:01 -0700

    reconstruction done

--- src/components/Rotom.js
+++ src/components/Rotom.js
@@ -11,14 +11,27 @@ const Rotom = () => {
                 <div class="dexneck"></div>
             </div>
             <div class="dexbody">
-            <div class="rotom_head">
-                
-            </div>
-
+                <div class="rotom_head">
+                    <div class="left_eye">
+                        <div class="left_slit"></div>
+                        <div class="left_eyeris"></div>
+                    </div>
+                    <div class="right_eye">
+                        <div class="right__slit"></div>
+                        <div class="right_eyeris"></div>
+                    </div>
+                    <div class="rotom_mouth">
+                        <div class="rotom_mouth_left"></div>
+                        <div class="rotom_mouth_center"></div>
+                        <div class="rotom_mouth_right"></div>
+                    </div>
+                </div>
             </div>  
                 <div class="dextail"></div>
             </div>
-        <p className='rotom-blurb'>This pokeball is CSS art im uniquely proud of</p>
+        <p className='rotom-blurb'>This pokeball is CSS art im uniquely proud of <br/> 
+        <img src={process.env.PUBLIC_URL + '/rotom-dex.jpg'} alt='Rotom for refrence' className='rotom-pic'></img>
+        </p>
     </div>
     );
 }
