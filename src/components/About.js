e2d041ad3bb0e6b250e50d37511a519f2e50b11a
Shandem <gutierrezjoshuah@gmail.com>
2022-04-06 15:47:33 -0700

    new title

--- src/components/About.js
+++ src/components/About.js
@@ -12,8 +12,8 @@ const About = () => {
             <img src={process.env.PUBLIC_URL + '/me.jpg'} className='me'alt='Self-Portrait'/>
            
             <p className='blurb-a'>Hi, I'm Joshuah Gutierrez, a graduate of General 
-            Assembly's Software Engineering Immersive course. I am currently with 
-            Enki.Ai as a Full Stack Developer. Before that, I had seven years of experience 
+            Assembly's Software Engineering Immersive course. I am currently a Lead apprentice with Alpha Works
+            for their React Cohort. Before that, I had seven years of experience 
             in the hospitality area. With four of those years in a management position. I am a 
             lifelong gamer and snowboarder.  It's not a job if it's fun!</p>
         </div>
