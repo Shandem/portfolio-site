d1b12e09dddcc426a0b643fa4310996d2925e090
Shandem <gutierrezjoshuah@gmail.com>
2023-07-21 15:24:27 -0700

    fixed the whole 1st tab of links for portfoilio in landscapme for ipad air

--- src/components/Gaymeit.js
+++ src/components/Gaymeit.js
@@ -7,33 +7,39 @@ const Gaymeit = () => {
         <div className='project gaymeitcontain'>
             <img src={process.env.PUBLIC_URL + '/gayme-it.png'} className='screenshot gaymeit'alt='Screen Shot of Pokemon Trading Car Game clone'></img>
                 <div className='detailContainer'>
+                <div className='gaymeitBlurb'>
+                <div class="cardHeader">
                     <h4>Gayme-it: A Reddit clone for Gaymers</h4>
                     <h5>Solo-project Full stack MERN App <br/>
                     Technologies Used: Full-stack MERN app Mongo, Express, React, Node, CSS</h5>
+                </div>
+                <p>The goal of this project was to create a Reddit-clone, focusing on 
+                    the implementation of karma voting. The Server for this React app was 
+                    set up as an API.
+                    </p>
+                </div>
+           
                     <h6 className='gaymeit-icons'>
                         <Icon url='https://github.com/Shandem/Gaymer-api'
                         linkClassName='github'
                         img='/Icons/catopuss.png'
                         imgClassName='catopuss-icon icons'
-                        linkText='API'
+                        
                         />
                         <Icon url='https://github.com/Shandem/Gyamer-client'
                         linkClassName='github'
                         img='/Icons/GitHub_Logo.png'
                         imgClassName='github-icon gicon icons'
-                        linkText='Client'
+                       
                         />
                         <Icon url='https://gayme-it.herokuapp.com'
                         linkClassName='browser'
                         img='/Icons/open-in-toon.png'
                         imgClassName='browser-icon icons'
-                        linkText='Live'
+                      
                         />
                     </h6>
-                    <p>The goal of this project was to create a Reddit-clone, focusing on 
-                    the implementation of karma voting. The Server for this React app was 
-                    set up as an API.
-                    </p>
+              
                 </div>
         </div>
     );
--- src/components/Pokeball.js
+++ src/components/Pokeball.js
@@ -5,16 +5,17 @@ const Pokeball = () => {
     return (
         <div className='cssart'>
             <div className="pokeball">  
+
                 <div id="pokeball">
                     <div id="layer1">
                         <div id="center"></div>
                         <div id="bottom-half"></div>
                     </div>  
                 </div>
+
             </div>
             <p className='blurb bblurb'>Here is some custom CSS art I put together. It is a recreation of 
-            Nintendo's iconic Pokeball from the Pokemon franchise. I have provided
-            an image of an official Pokeball for your reference <br/>
+            Nintendo's iconic Pokeball from the Pokemon franchise. <br/>
             
             </p>
         </div>
